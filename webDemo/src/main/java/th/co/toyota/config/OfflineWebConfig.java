10168b5c575c505399944144e837fdeab54b97ba
Chairat Samphaothong <chairat.it08@gmail.com>
2018-07-04 17:51:14 +0700

    success add profile and servlet filter to spring boot app

--- webDemo/src/main/java/th/co/toyota/config/OfflineWebConfig.java
+++ webDemo/src/main/java/th/co/toyota/config/OfflineWebConfig.java
@@ -2,8 +2,8 @@ package th.co.toyota.config;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import javax.servlet.Filter;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +11,13 @@ import org.springframework.context.annotation.Profile;
 
 import th.co.toyota.sc2.client.offline.CSC22110OfflineFilter;
 
-import javax.servlet.Filter;
-
-@Profile(value="OFFLINE")
+@Profile("OFFLINE")
 @Configuration
 public class OfflineWebConfig {
-	@Autowired
-	private AutowireCapableBeanFactory beanFactory;
 	@Bean
 	FilterRegistrationBean offlineLoginFilterRegistration() {
 		FilterRegistrationBean frb = new FilterRegistrationBean();
 		Filter standardOfflineFilter = new CSC22110OfflineFilter();
-		beanFactory.autowireBean(standardOfflineFilter);
 		frb.setFilter(standardOfflineFilter);
 		frb.setName("SC2 Offline Filter");
 		frb.setUrlPatterns(Arrays.asList("/*"));
